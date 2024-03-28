7902e629f96787c7b581984028cdf9e690249bb8
Ufuk Emre Yücetürk <ufukemreyuceturk@hotmail.com>
2024-03-29 02:11:35 +0300

    ChatPage.js ve ChatPage.module.css dosyalarında yapılan değişiklikler

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -2,47 +2,91 @@
 
 import React, { useState } from 'react';
 import styles from './ChatPage.module.css';
+import sendIcon from './icons8-send-letter-50.png';
+import logo from './img_passific_house_logo.png';
+import userPhoto from './icons8-user-48.png';
 
 const ChatPage = () => {
+  const [messages, setMessages] = useState([]);
   const [question, setQuestion] = useState('');
-  const [answer, setAnswer] = useState('');
 
   const handleQuestionSubmit = () => {
-    // Soruyu gönderme işlemleri burada gerçekleştirilebilir
-    // Örneğin: API çağrısı yapılabilir
+    if (question) {
+      setMessages(prevMessages => [...prevMessages, {type: "question", text: question}]);
+      setQuestion('');
+      request(question).then((data) => {
+        setMessages(prevMessages => [...prevMessages, {type: "answer", text: data.text}]);
+      });
+    }
   };
+
+  const handleNewChat = () => {
+    setMessages([]);
+  }
+
+  const request = async (question) => {
+    try {
+      //TODO: Uncomment the following code to make a request to the server
+      //const response = await fetch('http://localhost:5000/chat', {
+      //  method: 'POST',
+      //  headers: {
+      //    'Content-Type': 'application/json',
+      //  },
+      //  body: JSON.stringify({ question }),
+      //});
+      //const data = await response.json();
+      const data = await { type: "answer", text: "This is an answer"}
+      return data;
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
 
   return (
     <div className={styles.pageContainer}>
       <div className={styles.leftPanel}>
-        <img src="logo.png" alt="Logo" className={styles.logo} />
-        <button className={styles.newChatButton}>New Chat</button>
-        <div className={styles.userInfo}>
-          <img src="user-photo.jpg" alt="User" className={styles.userPhoto} />
-          <span className={styles.userName}>John Doe</span>
+        <div className={styles.leftPanelTop}>
+          <img src={logo} alt="Logo" className={styles.logo} />
+          <button onClick={handleNewChat} className={styles.newChatButton}>New Chat</button>
         </div>
-        <button className={styles.logoutButton}>Logout</button>
-      </div>
-      <div className={styles.chatScreen}>
-        <div className={styles.question}>
-          <label className={styles.label}>Question:</label>
-          <p>{question}</p>
+        <div className={styles.leftPanelBottom}>
+          <div className={styles.userInfo}>
+            <img src={userPhoto} alt="User" className={styles.userPhoto} />
+            <span className={styles.userName}>John Doe</span>
+          </div>
+          <button className={styles.logoutButton}>Logout</button>
         </div>
-        <div className={styles.answer}>
-          <label className={styles.label}>Answer:</label>
-          <p>{answer}</p>
+        
+      </div>
+      <div className={styles.rightPanel}>
+        <div className={styles.chatScreen}>
+        {messages.length === 0 && <p className={styles.welcomeMessage}>How can I help you today?</p>}
+        {messages.map((message, index) => (
+          <div key={index} className={message.type === "question" ? styles.question : styles.answer}>
+            
+            <label className={styles.label}>
+              {message.type === "question" ? "Question:" : "Pasific AI:"}
+            </label>
+            <p>{message.text}</p>
+          </div>
+        ))}
+        </div>
+        <div className={styles.questionInput}>
+          <input
+            type="text"
+            placeholder="Type your question..."
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+          <button className={styles.submitQuestionButton} onClick={handleQuestionSubmit}>
+            <img src={sendIcon} style={{
+              width: 30,
+              height: 30
+            }} alt="Send" />
+          </button>
         </div>
       </div>
-      <div className={styles.questionInput}>
-        <input
-          type="text"
-          placeholder="Type your question..."
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-        />
-        <button onClick={handleQuestionSubmit}>Send</button>
-      </div>
-   
     </div>
   );
 };
